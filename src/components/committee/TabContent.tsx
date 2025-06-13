413ffeefba9d8a80a00777726ca87b41584787c1
Nandan Pai <nandanrp09@gmail.com>
2025-06-13 18:56:04 +0530

    fix: minor ui change

--- src/components/committee/TabContent.tsx
+++ src/components/committee/TabContent.tsx
@@ -72,7 +72,7 @@ export default function TabContent({ tabId }: TabContentProps) {
               </p>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {steeringCommittee.map((member, index) => (
                 <div
                   key={index}
